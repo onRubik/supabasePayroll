03993483860d1397dfe330185d18b1b716fe1bd9
onRubik <lifesonrubik@gmail.com>
2021-05-14 14:28:31 -0600

    linux changes

--- src/js/vacPerPeriod.js
+++ src/js/vacPerPeriod.js
@@ -1,3 +1,6 @@
+//Date.prototype.getFullYear()
+//Date.parse()
+
 const {Client} = require('pg');
 
 const connectionData = {
@@ -16,6 +19,13 @@ client.query('SELECT hiredate FROM employees WHERE employeeid = 19838').then(
     response => {
         qObject = response.rows;
         console.log(qObject);
+        
+        
+        // when running the next two lines is gets typeerror: this is not a date object
+        let yearTest = Date.prototype.getFullYear(qObject);
+        console.log(yearTest);
+        
+        
         //test to manipulate the year
         //let newDate = new Date();
         // newDate.setFullYear(qObject.getFullYear()+1);
