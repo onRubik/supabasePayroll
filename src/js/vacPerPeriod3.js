0f7c7aa9061a98272a2473a4bc99531ce8a9bafe
onRubik <lifesonrubik@gmail.com>
2021-05-15 20:10:42 -0600

    no comment

--- src/js/vacPerPeriod3.js
+++ src/js/vacPerPeriod3.js
@@ -19,6 +19,17 @@ client.connect();
 client.query(query).then(
   response => {
       console.log(response.rows);
+      console.log('type: ' + typeof response.rows)
+      
+      const parsedInfo = Date.parse(response.rows);
+      console.log(parsedInfo);
+      console.log('dateType: ' + parsedInfo);
+      //to string:
+      console.log(parsedInfo.toString());
+      //console.log(datetime.toISOString().slice(0,10));
+      console.log(parsedInfo.toISOString());
+      
+      
       client.end();
   })
   .catch(err => {
