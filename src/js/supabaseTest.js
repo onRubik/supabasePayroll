cc2d636d1a2f2a2f504e1fbb2e1373867775b055
onRubik <lifesonrubik@gmail.com>
2021-05-19 14:39:31 -0600

    supabase api included

---
+++ src/js/supabaseTest.js
@@ -0,0 +1,25 @@
+const infoObject = require('./supabaseUrlKey');
+const {createClient} = require('@supabase/supabase-js')
+
+const supabase = createClient(infoObject.supabaseUrl, infoObject.supabaseAnonKey)
+
+// supabase
+//     .from('employees')
+//     .select('*')
+//     .then(console.log)
+//     .catch(console.error)
+
+const main = async () => {
+    let {data, error} = await supabase
+        .from('employees')
+        .select('*')
+    
+    if (error){
+        console.error(error)
+        return
+    }
+
+    console.log(data)
+}
+
+main()
