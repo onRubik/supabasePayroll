0791e913ee43a23cf55ed800113112dc6d097682
onRubik <lifesonrubik@gmail.com>
2021-05-22 20:20:26 -0600

    github first release

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const newDml = require('./src/js/dml')
 
-// newDml.selectEventDesc('PP')
-// newDml.currentPeriod(10490)
-// newDml.yearsInService(10490)
+//to test specific DML operations comment or uncomment the next code lines
 
+//Select from the records 'eventRecords' per description
+newDml.selectEventDesc()
+
+//Find the current yeard period for an employee
+newDml.currentPeriod()
+
+//Find the number of years in service for an employee
+newDml.yearsInService(10490)
+
+//Count events per current period, for an employee
+newDml.selectEventCurrentPeriod()
+
+//Rest the vacations available for the current period, for an emplpyee, minus the vacations taken in the current period
+newDml.vacAvailable()
--- src/js/dml.js
+++ src/js/dml.js
@@ -1,8 +1,16 @@
+//to store the supabase Url and anon Key dave a .js file namded "supabaseUrlKey" with the next data structure
+//module.exports = {
+//    supabaseUrl: "",
+//    supabaseAnonKey: ""
+//}
+
+
 const infoObject = require('./supabaseUrlKey')
 const {createClient} = require('@supabase/supabase-js')
 var moment = require('moment')
 
 const supabase = createClient(infoObject.supabaseUrl, infoObject.supabaseAnonKey)
+
 
 const selectEventDesc = async (eventDesc) => {
     let {data, error} = await supabase
@@ -17,11 +25,72 @@ const selectEventDesc = async (eventDesc) => {
     console.log(data)
 }
 
-const currentPeriod = async (empId) => {
+
+const selectEventCurrentPeriod = async (Id, desc) => {
+    let gt = new Date()
+    let lt = new Date()
+
+    
+    let period = await currentPeriod(Id)
+    
+    async function ifCondition (){
+        if(period.today.getTime() === period.today2.getTime()){
+            gt = ( String(period.today.getFullYear()) + String(period.today.getMonth()) + String(period.today.getDate()))
+            lt = period.today.getFullYear() + 1   
+            
+            let {data, error} = await supabase
+                .from('eventRecords')
+                .select('*')
+                .eq('employeeId', Id)
+                .like('eventDesc', desc)
+                .gt('eventDate', gt)
+                .lt('eventDate', lt)
+        
+            if (error){
+                console.error(error)
+                return
+            }
+
+            let daysLenght = Object.keys(data).length
+            
+            // console.log(daysLenght)
+            return daysLenght
+        }
+    
+        if(period.today.getTime() > period.today2.getTime()){
+            gt = ( String(period.today.getFullYear()-1)+ '-' + String(period.today.getMonth()) + '-' + String(period.today.getDate()))
+            lt = ( String(period.today.getFullYear()) + '-' + String(period.today.getMonth()) + '-' + String(period.today.getDate()))   
+            let {data, error} = await supabase
+                .from('eventRecords')
+                .select('*')
+                .eq('employeeId', Id)
+                .like('eventDesc', desc)
+                .gt('eventDate', gt)
+                .lt('eventDate', lt)
+
+            if (error){
+                console.error(error)
+                return
+            }
+
+            let daysLenght = Object.keys(data).length
+            
+            // console.log(daysLenght)
+            return daysLenght
+        }
+
+    }
+
+    let returtnResult = await ifCondition()
+    return returtnResult
+}
+
+
+const currentPeriod = async (Id) => {
     let {data, error} = await supabase
         .from('employees')
         .select('hiredate')
-        .eq('employeeId', empId)
+        .eq('employeeId', Id)
 
     if(error){
         console.error(error)
@@ -43,12 +112,12 @@ const currentPeriod = async (empId) => {
 
     if(today.getTime() === today2.getTime()){
         let period = (today.getFullYear() + ' - ' + today.getFullYear() + 1)
-        console.log("current period: " + period)
+        // console.log("current period: " + period)
     }
 
     if(today.getTime() > today2.getTime()){
         let period = ((today.getFullYear())-1 + ' - ' + today.getFullYear())
-        console.log("current period: " + period)
+        // console.log("current period: " + period)
     }
 
     return {
@@ -58,20 +127,71 @@ const currentPeriod = async (empId) => {
     }
 }
 
+
 const yearsInService = async (Id) => {
     let hireDate = await currentPeriod(Id)
     console.log('years in service: ' + moment.duration(hireDate.today2 - hireDate.newDate).years())
+    let years = moment.duration(hireDate.today2 - hireDate.newDate).years()
     
+    return years
 }
 
-const vacPerPeriod = async () => {
+
+const vacPerPeriod = async (Id) => {
+    let years = await yearsInService(Id)
+    let {data, error} = await supabase
+        .from('vacationsPolicy')
+        .select('daysPerYear')
+        .eq('years', years)
+
+    if(error){
+        console.error(error)
+        return
+    }
+
+    let yearsObject = new Object()
+    yearsObject = data[0]
+    years = (Object.values(yearsObject))
+    console.log('vacation days per year in service: ' + years)
+
+    return years
+}
+
+
+const countEventDesc = async (desc, Id) => {
+    let {data, error} = await supabase
+        .from('eventRecords')
+        .select('lastName, eventDate, eventDesc')
+        .like('eventDesc', desc)
+        .eq('employeeId', Id)
+    
+    if (error){
+        console.error(error)
+        return
+    }
+
+    let lenght =  Object.keys(data).length
+
+    return lenght
+}
+
+
+const vacAvailable = async (Id, desc) => {
+    let lenght = await selectEventCurrentPeriod(Id, desc)
+    let available = await vacPerPeriod(Id)
+
+    const result = available - parseInt(lenght)
+
+    console.log('vacations available: ' + result)
 
 }
 
 
 module.exports = {
     selectEventDesc,
+    selectEventCurrentPeriod,
     currentPeriod,
     yearsInService,
-    vacPerPeriod
+    countEventDesc,
+    vacAvailable
 }
